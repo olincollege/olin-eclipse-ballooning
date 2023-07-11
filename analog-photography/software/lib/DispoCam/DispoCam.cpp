43f5a8ac84d0f847d5bcd5501bb1e9d1122c522f
Jacob Smilg <jacob@smilg.dev>
2023-07-11 14:17:00 -0400

    move camera code to library to simplify having multiple cameras

---
+++ analog-photography/software/lib/DispoCam/DispoCam.cpp
@@ -0,0 +1,78 @@
+#include "DispoCam.h"
+
+DispoCam::DispoCam(Servo *windServo, Servo *shutterServo, Debounce *limitSwitch) :
+    windServo(windServo), shutterServo(shutterServo), limitSwitch(limitSwitch),
+    state(!digitalRead(limitSwitch->getPin()) ? WOUND : UNWOUND)
+{
+    windServo->write(windServoStop);
+    shutterServo->write(shutterServoHome);
+}
+
+void DispoCam::readyNext() {
+    nextStateReady = !servoActive();
+}
+
+bool DispoCam::servoActive() {
+    return (state == WINDING || state == PRESSING);
+}
+
+void DispoCam::update() {
+    limitSwitch->poll();
+
+    // FSM for winding film and taking pictures
+    switch (state) {
+    case UNWOUND:   // reset shutter servo and wait some amount of time
+        // ACTION: reset shutter servo (move to home/idle position)
+        shutterPos = shutterServoHome;
+        shutterServo->write(shutterPos);
+        // STATE CHANGE: if enough time passed → WINDING
+        if ((millis() - lastShutterTime) > windDelay_ms && nextStateReady) {
+            state = WINDING;
+            nextStateReady = false;
+            // DEBUG_SERIAL.println("UNWOUND -> WINDING");
+        }
+        break;
+    case WINDING:   // wind film until it's wound (switch closes)
+        // ACTION: turn wind servo, poll limit switch
+        windServo->write(windServoStop + windServoSpeed);
+
+        // STATE CHANGE: if wind switch is closed → WOUND
+        if (limitSwitch->getState()) {
+            state = WOUND;
+            nextStateReady = false;
+            // DEBUG_SERIAL.println("WINDING -> WOUND");
+        }
+        break;
+    case WOUND:     // done winding, stop the servo and wait to take picture
+        // ACTION: stop wind servo
+        windServo->write(windServoStop);
+
+        // STATE CHANGE: if trigger button pressed → PRESSING
+        if (nextStateReady) {
+            state = PRESSING;
+            nextStateReady = false;
+            // DEBUG_SERIAL.println("WOUND -> PRESSING");
+        }
+        break;
+    case PRESSING:  // take a picture
+        // ACTION: incrementally move shutter servo
+        shutterPos += shutterServoIncrement;
+        if (shutterPos >= 170) {    // constrain servo angle to 10-170°
+            shutterPos = 170;
+        } else if (shutterPos < 10) {
+            shutterPos = 10;
+        }
+        shutterServo->write(shutterPos);
+        
+        // STATE CHANGE: wind switch open → UNWOUND
+        if (!limitSwitch->getState()) {
+            state = UNWOUND;
+            lastShutterTime = millis(); // reset timer
+            nextStateReady = false;
+            // DEBUG_SERIAL.println("PRESSING -> UNWOUND");
+        }
+        break;
+    default:
+        break;
+    }
+}
---
+++ analog-photography/software/lib/DispoCam/DispoCam.h
@@ -0,0 +1,41 @@
+#ifndef DispoCam_h
+#define DispoCam_h
+#include "Arduino.h"
+#include "Debounce.h"
+#include "Servo.h"
+
+const int windDelay_ms = 1000;  // time in ms to wait before winding film
+
+const int windServoStop = 90;   // "zero" position for servo to stop moving
+const int windServoSpeed = 20;  // added to stop position for moving servo
+const int shutterServoHome = 90;
+const int shutterServoIncrement = 10;
+
+enum CAM_STATES {
+    UNWOUND,    // idle; limit open
+    WINDING,    // wind servo active
+    WOUND,      // servos stopped
+    PRESSING,   // shutter servo active
+};
+
+class DispoCam {
+    private:
+        enum CAM_STATES state;
+        int limitPin;
+        int shutterPos = shutterServoHome;
+        unsigned long lastShutterTime = 0;
+        Debounce *limitSwitch;
+        bool nextStateReady = false;
+        Servo *windServo;
+        Servo *shutterServo;
+
+    public:
+        DispoCam(Servo *windServo, Servo *shutterServo, Debounce *limitSwitch);
+        void update();  // poll switches, update FSM
+        bool servoActive(); // true if in a state where a servo is moving
+        CAM_STATES getState();
+        void readyNext();
+
+};
+
+#endif
