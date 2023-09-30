8e91b19dcc108df407093fe1221a3c3970ad7b56
Jacob Smilg <jacobasmilg@gmail.com>
2023-09-30 16:12:09 -0400

    wait for input if error

--- iridium-control/main.py
+++ iridium-control/main.py
@@ -5,16 +5,19 @@ from gui import EmailSenderGUI
 from email_api import EmailSender
 
 if __name__ == '__main__':
-    load_dotenv()
-    from_email = os.environ.get('FROM')
-    imei = os.environ.get('IMEI')
-    if from_email is None or imei is None:
-        raise NameError('Error: .env configuration not set! Make sure to '
-                        'add the FROM email address and your IMEI to .env.')
-    if not os.path.exists('attachments'):
-        print('attachments not found! place .sbd attachments in the attachments/ directory.')
-        os.mkdir('attachments')
-    sender = EmailSender(imei, from_email)
-    root = Tk()
-    EmailSenderGUI(sender, root)
-    root.mainloop()
+    try:
+        load_dotenv()
+        from_email = os.environ.get('FROM')
+        imei = os.environ.get('IMEI')
+        if from_email is None or imei is None:
+            raise NameError('Error: .env configuration not set! Make sure to '
+                            'add the FROM email address and your IMEI to .env.')
+        if not os.path.exists('attachments'):
+            print('attachments not found! place .sbd attachments in the attachments/ directory.')
+            os.mkdir('attachments')
+        sender = EmailSender(imei, from_email)
+        root = Tk()
+        EmailSenderGUI(sender, root)
+        root.mainloop()
+    except:
+        input()
